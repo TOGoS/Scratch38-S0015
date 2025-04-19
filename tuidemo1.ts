3ad7b6d9c98e4b80cc05cd0ad8562bdbd383a025
TOGoS <togos00@gmail.com>
2025-04-19 21:18:03 +0000

    Refactoring, more care w/ state

--- tuidemo1.ts
+++ tuidemo1.ts
@@ -15,38 +15,58 @@ let colorIndex = 0;
 
 const textEncoder = new TextEncoder();
 
-function clear() : Promise<unknown> {
-	return Deno.stdout.write(textEncoder.encode('\x1b[0J'));
-}
 function moveToTop() : Promise<unknown> {
 	return Deno.stdout.write(textEncoder.encode('\x1b[1;1H'));
 }
-function drawLog(str:string) : Promise<unknown> {
+function printLine(str:string) : Promise<unknown> {
 	return Deno.stdout.write(textEncoder.encode(str+"\n"));
 }
 
-let inTui : boolean = false;
-let messages : string[] = [];
+function padLeft(template:string, content:string) : string {
+	if( content.length > template.length ) return content.substring(content.length-template.length);
+	return template.substring(0, template.length-content.length) + content;
+}
 
-async function drawBox() {
+let state : "default"|"entering-tui"|"tui"|"exiting-tui" = "default";
+let messages : string[] = [];
+let redrawRequested = false;
+let drawCount = 0;
+
+// TODO: Less asynchronicity; one 'thread' (i.e. promise chain) for all output changes and writing
+
+async function draw() {
+	if( state != "tui" ) return;
+	
+	++drawCount;
+	
 	await moveToTop();
 	
 	const nocolor = '\x1b[0m';
 	const color = colors[colorIndex];
-	await drawLog(`┌────────────┐`);
-	await drawLog(`│ ${color}Hello TUI!${nocolor} │`);
-	await drawLog(`└────────────┘`);
+	// See https://en.wikipedia.org/wiki/Box-drawing_characters
+	await printLine(`┌───────────────┐`);
+	await printLine(`│    ${color}Hello TUI!${nocolor} │`);
+	await printLine(`├───────────────┤`);
+	await printLine(`│ Draws: \x1b[34m${padLeft("      ", ""+drawCount)}${nocolor} │`);
+	await printLine(`└───────────────┘`);
 	for( const m of messages ) {
-		drawLog(m);
+		printLine(m);
 	}
 }
 
 function requestRedraw() {
-	drawBox();
+	if( redrawRequested ) return;
+	
+	redrawRequested = true;
+	setTimeout(async () => {
+		await draw();
+		redrawRequested = false;
+	}, 0);
 }
 
 function log(message:string) {
-	if( inTui ) {
+	// TODO: Just have a logger variable that can be switched out when exiting
+	if( state == "entering-tui" || state == "tui" ) {
 		messages.push(message);
 		messages = messages.slice(Math.max(0,messages.length - 10));
 		requestRedraw();
@@ -61,20 +81,23 @@ function updateColor() {
 }
 
 async function enterTui() {
-	inTui = true;
-	await Deno.stdin.setRaw(true);
+	state = "entering-tui";
 	await Deno.stdout.write(textEncoder.encode(ansicodes.USE_SECONDARY_BUFFER + ansicodes.HIDE_CURSOR));
+	await Deno.stdout.write(textEncoder.encode(ansicodes.CLEAR_SCREEN));
+	await Deno.stdin.setRaw(true);
+	state = "tui";
 }
 
 async function exitTui() {
+	state = "exiting-tui";
 	await Deno.stdin.setRaw(false);
 	await Deno.stdout.write(textEncoder.encode(ansicodes.USE_PRIMARY_BUFFER + ansicodes.SHOW_CURSOR));
-	inTui = false;
+	state = "default";
 }
 
 await enterTui();
-drawLog("Lawg!");
-drawBox();
+printLine("Lawg!");
+draw();
 const interval = setInterval(updateColor, 1000);
 
 const input = Deno.stdin.readable;
@@ -87,8 +110,11 @@ async function quit() {
 
 async function* inputEvents(stdin : ReadableStream) {
 	for await( const chunk of stdin ) {
+		// TODO: Roll own decoding; this one merges multiple keystrokes into one;
+		// typing "escape" really fast is indistinguishable from the event
+		// representation of the "escape" key!
 		for (const event of decodeBuffer(chunk)) {
-			yield event;
+			yield {key: event.key, ctrl: event.ctrl, meta: event.meta, shift: event.shift};
 		}
 	}
 }
@@ -108,23 +134,6 @@ try {
 		throw e;
 	}
 }
-
-// See tui/src/input_reader/mod.ts for hints on handling input
-/*
-for await( const buf of Deno.stdin.readable ) {
-	for( const byte of buf ) {
-		if( byte == 113 ) { // 'q'
-			console.log(`# Bye bye!`);
-			quit();
-		} else {
-			exitTui();
-			console.log(`# You said ${byte}; Goodbye!`);
-			Deno.exit(0);
-		}
-	}
-}
-*/
-	
 
 // Note: If debugging in the 'debug console' of Visual Studio Code, you'll get console.whatever,
 // but not things written to Deno.stdout!  Bah!
