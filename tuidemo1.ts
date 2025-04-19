51d03a2cd92439d4863bd0f1b50d4c08caec3729
TOGoS <togos00@gmail.com>
2025-04-19 06:17:00 +0000

    Notes to self

--- tuidemo1.ts
+++ tuidemo1.ts
@@ -10,19 +10,38 @@ const colors = [
 
 let colorIndex = 0;
 
-function drawBox() {
-	console.clear();
-	const nocolor = '\x1b[0m';
-	const color = colors[colorIndex];
-	console.log(`┌────────────┐`);
-	console.log(`│ ${color}Hello TUI!${nocolor} │`);
-	console.log(`└────────────┘`);
+const textEncoder = new TextEncoder();
+
+function clear() : Promise<unknown> {
+	return Deno.stdout.write(textEncoder.encode('\x1b[0J'));
+}
+function moveToTop() : Promise<unknown> {
+	return Deno.stdout.write(textEncoder.encode('\x1b[1;1H'));
+}
+function lawg(str:string) : Promise<unknown> {
+	return Deno.stdout.write(textEncoder.encode(str+"\n"));
 }
 
+async function drawBox() {
+	await moveToTop();
+	
+	const nocolor = '\x1b[0m';
+	const color = colors[colorIndex];
+	await lawg(`┌────────────┐`);
+	await lawg(`│ ${color}Hello TUI!${nocolor} │`);
+	await lawg(`└────────────┘`);
+}
 function updateColor() {
 	colorIndex = (colorIndex + 1) % colors.length;
 	drawBox();
 }
 
+console.log("Clearing screen...");
+console.clear();
+console.log("DONE CLEARING");
+lawg("Lawg!");
 drawBox();
 setInterval(updateColor, 1000);
+
+// Note: If debugging in the 'debug console' of Visual Studio Code, you'll get console.whatever,
+// but not things written to Deno.stdout!  Bah!
