09acbf358ea163ea84cac0d52f917a46019cf04e
TOGoS <togos00@gmail.com>
2025-04-21 00:29:36 +0000

    Box drawings

--- boxdrawr2.ts
+++ boxdrawr2.ts
@@ -103,3 +103,33 @@ Deno.test("3x3 box", () => {
 		"└─┘\n"
 	)
 });
+Deno.test("3x3 box w/ cross", () => {
+	const boxdrawr = new BoxDrawr(3, 3);
+	boxdrawr.addLine(2,2,2,0,BDC_PROP_VALUES.LIGHT);
+	boxdrawr.addLine(2,0,0,0,BDC_PROP_VALUES.LIGHT);
+	boxdrawr.addLine(0,0,0,2,BDC_PROP_VALUES.LIGHT);
+	boxdrawr.addLine(0,2,2,2,BDC_PROP_VALUES.LIGHT);
+	boxdrawr.addLine(0,1,2,1,BDC_PROP_VALUES.LIGHT);
+	boxdrawr.addLine(1,0,1,2,BDC_PROP_VALUES.LIGHT);
+	assertEquals(
+		boxdrawr.contentToString(),
+		"┌┬┐\n"+
+		"├┼┤\n"+
+		"└┴┘\n"
+	)
+});
+Deno.test("3x3 double box w/ cross", () => {
+	const boxdrawr = new BoxDrawr(3, 3);
+	boxdrawr.addLine(2,2,2,0,BDC_PROP_VALUES.DOUBLE);
+	boxdrawr.addLine(1,0,1,2,BDC_PROP_VALUES.DOUBLE);
+	boxdrawr.addLine(2,0,0,0,BDC_PROP_VALUES.DOUBLE);
+	boxdrawr.addLine(0,0,0,2,BDC_PROP_VALUES.DOUBLE);
+	boxdrawr.addLine(0,2,2,2,BDC_PROP_VALUES.DOUBLE);
+	boxdrawr.addLine(0,1,2,1,BDC_PROP_VALUES.DOUBLE);
+	assertEquals(
+		boxdrawr.contentToString(),
+		"╔╦╗\n"+
+		"╠╬╣\n"+
+		"╚╩╝\n"
+	)
+});
