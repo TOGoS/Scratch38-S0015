0dcd72daf03659099c0c04e3fc95490396d4fe2c
TOGoS <togos00@gmail.com>
2025-08-09 16:05:50 -0500

    Librarify tuidemo3 app framework as tuiappframework3.ts

--- src/demo/ts/tuidemo3.ts
+++ src/demo/ts/tuidemo3.ts
@@ -1,258 +1,17 @@
 import Vec2D from '../../lib/ts/termdraw/Vec2D.ts';
-import DrawCommand from "../../lib/ts/termdraw/DrawCommand.ts";
 
 import KeyEvent from '../../lib/ts/terminput/KeyEvent.ts';
-import TextRaster2, { Style } from '../../lib/ts/termdraw/TextRaster2.ts';
-import TUIRenderStateManager from '../../lib/ts/termdraw/TUIRenderStateManager.ts';
-import { inputEvents } from '../../lib/ts/terminput/inputeventparser.ts';
-import { blitToRaster, createUniformRaster, drawTextToRaster, textRaster2ToDrawCommands, textRaster2ToLines } from '../../lib/ts/termdraw/textraster2utils.ts';
+import { Style } from '../../lib/ts/termdraw/TextRaster2.ts';
+import { createUniformRaster, drawTextToRaster } from '../../lib/ts/termdraw/textraster2utils.ts';
 import * as ansi from '../../lib/ts/termdraw/ansi.ts';
-import { toAnsi } from '../../lib/ts/termdraw/ansi.ts';
-import { iterateAndReturn, mergeAsyncIterables } from '../../lib/ts/_util/asynciterableutil.ts';
-import { Signal } from 'https://deno.land/x/tui@2.1.11/mod.ts';
-import { reset } from 'https://deno.land/std@0.165.0/fmt/colors.ts';
+import { AbstractAppInstance, PossiblyTUIAppContext, PossiblyTUIAppSpawner, runTuiApp, Waitable } from '../../lib/ts/tuiappframework3.ts';
 
-//// Some types that could be librarified if this all works
-
-type PossiblyTUIAppSpawner<Context,Instance,InputEvent> = {
-	// Framework will push key events to the app 'asynchronously'
-	inputMode: "push-key-events",
-	spawn(context: Context) : Instance & {handleInput(input:InputEvent) : void};
-} | {
-	inputMode: "none",
-	spawn(context: Context) : Instance;
-};
-
-// ProcessLike, but more abstract
-interface Waitable<R> {
-	wait() : Promise<R>;
-}
-
-interface TUIAppInstance<I,R> extends Waitable<R> {
-	handleInput(input:I) : void;
-}
-
-interface PossiblyTUIAppContext {
-	// Hmm: Directly using Readable/WritableStreams might not be the most useful thing;
-	// will try and find out later.
-	stdin?   : ReadableStream<Uint8Array>;
-	//stdout : WritableStream<Uint8Array>;
-	//stderr : WritableStream<Uint8Array>;
-	writeOut(text:string) : Promise<void>;
-	setScene(scene:Rasterable) : void;
-}
-
-interface Rasterable {
-	toRaster(minSize:Vec2D<number>, maxSize:Vec2D<number>) : TextRaster2;
-}
-
-type AppInputEvent = KeyEvent
-
-
-class AbstractWaitable<Result> implements Waitable<Result> {
-	protected _resolve : (r:Result) => void = (res) => { throw new Error(`_resolve not yet initialized!`); };
-	protected _reject : (e:any) => void = (res) => { throw new Error(`_reject not yet initialized!`); };
-	protected _exited : boolean = false;
-	#exited : Promise<Result>;
-	
-	/** Close any opened resources, etc */
-	_cleanup() : Promise<void> {
-		return Promise.resolve();
-	}
-	
-	constructor() {
-		this.#exited = new Promise<Result>((resolve,reject) => {
-			this._reject = (e) => {
-				if( this._exited ) {
-					// Ignore it; only the first call counts.
-					return;
-				}
-				this._exited = true;
-				this._cleanup().finally(() => reject(e));
-			}
-			this._resolve = (result) => {
-				if( this._exited ) {
-					// Ignore it; only the first call counts.
-					return;
-				}
-				this._exited = true;
-				this._cleanup().finally(() => resolve(result));
-			};
-		});
-	}
-		
-	wait(): Promise<Result> {
-		return this.#exited;
-	}
-}
+//// Misc helper functions
 
 function sleep(ms:number) {
 	return new Promise((resolve,_reject) => {
 		setTimeout(resolve, ms);
 	});
-}
-
-async function runTuiApp<Result>(
-	spawner: PossiblyTUIAppSpawner<PossiblyTUIAppContext, Waitable<Result>, KeyEvent>,
-	ctx:{
-		stdin  : typeof Deno.stdin,
-		stdout : WritableStreamDefaultWriter
-	},
-	opts: {
-		outputMode: "screen"|"lines"
-	}
-) : Promise<Result> {
-	const textEncoder = new TextEncoder();
-	
-	function getScreenSize() : Vec2D<number> {
-		const cs = Deno.consoleSize();
-		return { x: cs.columns, y: cs.rows };
-	}
-	
-	// Output manager provides a common interface to manage terminal output,
-	// whether in screen or lines mode.
-	const outMan : {
-		enter() : Promise<void>;
-		exit() : Promise<void>;
-		writeOut(text:string) : Promise<void>;
-		setScene(scene:Rasterable) : void;
-	} = opts.outputMode == "lines" ? (() => {
-		let outProm = Promise.resolve();
-		
-		return {
-			enter() { return outProm; },
-			exit() { return outProm; },
-			writeOut(text:string) {
-				return outProm.then(() => ctx.stdout.write(textEncoder.encode(text)));
-			},
-			setScene(scene:Rasterable) {
-				const outCommands = textRaster2ToLines(scene.toRaster({x:0,y:0}, getScreenSize()));
-				for( const command of outCommands ) {
-					outProm = outProm.then(() => ctx.stdout.write(textEncoder.encode(toAnsi(command))));
-				}
-				// outProm = outProm.then(() => ctx.stdout.write(textEncoder.encode(RESET_FORMATTING)));
-			}
-		}
-	})() : (() => {
-		let currentScene : Rasterable = {
-			toRaster(size:Vec2D<number>) : TextRaster2 {
-				return createUniformRaster(size, " ", ansi.RESET_FORMATTING);
-			}
-		}
-		let outProm = Promise.resolve();
-		let inTui = false; // At whatever point in time outProm represents
-		
-		const renderStateMan = new TUIRenderStateManager(ctx.stdout, async (out) => {
-			await outProm;
-			const screenSize = getScreenSize();
-			const raster = currentScene.toRaster(screenSize, screenSize);
-			const outCommands = textRaster2ToDrawCommands(raster);
-			for( const command of outCommands ) {
-				await out.write(textEncoder.encode(toAnsi(command)));
-			}
-			// await ctx.stdout.write(textEncoder.encode(RESET_FORMATTING));
-		});
-		
-		function setMode(newInTui:boolean) : Promise<void> {
-			outProm =
-				newInTui == inTui ? outProm :
-				newInTui ? outProm.then(() => renderStateMan.enterTui()) :
-				           outProm.then(() => renderStateMan.exitTui());
-			inTui = newInTui;
-			return outProm;
-		}
-		
-		return {
-			enter: () => outProm,
-			exit : () => setMode(false),
-			async writeOut(text:string) {
-				await setMode(false);
-				await ctx.stdout.write(textEncoder.encode(text));
-			},
-			setScene(scene:Rasterable) {
-				currentScene = scene;
-				setMode(true);
-				renderStateMan.requestRedraw();
-			}
-		};
-	})();
-
-	let rawModeSet = false;
-	let outManActive = false;
-	
-	async function cleanup() {
-		if( rawModeSet ) {
-			ctx.stdin.setRaw(false);
-			rawModeSet = false;
-		}
-		if( outManActive ) {
-			await outMan.exit();
-			outManActive = false;
-		}
-	}
-	
-	// Finally block is not run when the Deno process is killed with Ctrl+c
-	// (i.e. not in 'raw input' mode, in which case the program handles it).
-	// But this signal handler does seem to be run.
-	// 
-	// I have not figured out how to clean up
-	// when Ctrl+C is pressed while the process
-	// is reading from stdin.
-	Deno.addSignalListener("SIGINT", cleanup);
-	
-	try {
-		await outMan.enter();
-		outManActive = true;
-		
-		let stdin : ReadableStream<Uint8Array>|undefined;
-		if( spawner.inputMode == "push-key-events" ) {
-			ctx.stdin.setRaw(true);
-			rawModeSet = true;
-			stdin = undefined;
-		} else {
-			stdin = ctx.stdin.readable;
-		}
-		
-		const appInstance = spawner.spawn({
-			stdin   : stdin,
-			writeOut: outMan.writeOut,
-			setScene: outMan.setScene,
-		});
-		
-		// App is started!
-		
-		if( spawner.inputMode == "push-key-events" ) {
-			const reader = async () => {
-				const inputHandler = appInstance as unknown as {handleInput(evt:KeyEvent):void};
-				for await( const inputEvent of inputEvents(ctx.stdin.readable) ) {
-					inputHandler.handleInput(inputEvent);
-				}
-			};
-			reader(); // TODO: Cancel somehow when app exits
-		}
-		
-		const exitCode = await appInstance.wait();
-		return exitCode;
-	} finally {
-		await cleanup();
-	}
-}
-
-//// Apps!
-
-abstract class AbstractAppInstance<Input,Result> extends AbstractWaitable<Result> implements TUIAppInstance<Input,Result> {
-	protected _ctx : PossiblyTUIAppContext;
-	
-	constructor(ctx:PossiblyTUIAppContext) {
-		super();
-		this._ctx = ctx;
-	}
-	
-	_handleRunResult(prom:Promise<Result>) {
-		prom.then(this._resolve, this._reject);
-	}
-	
-	handleInput(_rejectinput: Input): void {}
 }
 
 function clamp(val:number, min:number, max:number) : number {
@@ -267,17 +26,18 @@ function clampSize(val:Vec2D<number>, min:Vec2D<number>, max:Vec2D<number>) : Ve
 }
 
 class DemoAppInstance extends AbstractAppInstance<KeyEvent,number> {
-	_requestCleanExit() {
+	_requestCleanExit(result:number) {
 		// could be overridden to say goodbye first or use a signal or something idk
-		this._resolve(0);
+		this._resolve(result);
 	}
+	// deno-lint-ignore no-explicit-any
 	_abort(reason:any) {
 		this._reject(reason);
 	}
 	
 	override handleInput(input:KeyEvent) {
 		if( input.key == "q" ) {
-			this._requestCleanExit();
+			this._requestCleanExit(0);
 		}
 		if(input.key == "c" && input.ctrlKey) {
 			this._abort(new Error("Aborted by user"));
@@ -586,7 +346,6 @@ function parseMain(args:string[]) : Spawner<ProcLikeSpawnContext,Waitable<number
 	} else if( topArgs.appName == "wc" ) {
 		const inputFiles : string[] = [];
 			for( const arg of topArgs.appArgs ) {
-				let m : RegExpExecArray|null;
 				if( arg == '-' || !arg.startsWith("-") ) {
 					inputFiles.push(arg);
 					// Means 'wc should read from stdin'.
@@ -606,7 +365,6 @@ function parseMain(args:string[]) : Spawner<ProcLikeSpawnContext,Waitable<number
 	}
 }
 
-// TODO: Put all the frameworky stuff in a library
 // TODO: Some kind of component library that
 //   automatically lays things out, can draw boxes, tables, etc
 
---
+++ src/lib/ts/tuiappframework3.ts
@@ -0,0 +1,242 @@
+import Vec2D from '../../lib/ts/termdraw/Vec2D.ts';
+
+import KeyEvent from '../../lib/ts/terminput/KeyEvent.ts';
+import TextRaster2 from '../../lib/ts/termdraw/TextRaster2.ts';
+import TUIRenderStateManager from '../../lib/ts/termdraw/TUIRenderStateManager.ts';
+import { inputEvents } from '../../lib/ts/terminput/inputeventparser.ts';
+import { createUniformRaster, textRaster2ToDrawCommands, textRaster2ToLines } from '../../lib/ts/termdraw/textraster2utils.ts';
+import * as ansi from '../../lib/ts/termdraw/ansi.ts';
+import { toAnsi } from '../../lib/ts/termdraw/ansi.ts';
+
+//// Some types that could be librarified if this all works
+
+export interface InputHandler<InputEvent> {
+	handleInput(input:InputEvent) : void;
+}
+
+/** Some running process that can be waited on to complete with a result */
+export interface Waitable<R> {
+	wait() : Promise<R>;
+}
+
+export interface Rasterable {
+	toRaster(minSize:Vec2D<number>, maxSize:Vec2D<number>) : TextRaster2;
+}
+
+/** Conext that can be used to instantiate both TUI and not-quite-TUI apps */
+export interface PossiblyTUIAppContext {
+	// Hmm: Directly using Readable/WritableStreams might not be the most useful thing;
+	// will try and find out later.
+	stdin?   : ReadableStream<Uint8Array>;
+	//stdout : WritableStream<Uint8Array>;
+	//stderr : WritableStream<Uint8Array>;
+	writeOut(text:string) : Promise<void>;
+	setScene(scene:Rasterable) : void;
+}
+
+export class AbstractWaitable<Result> implements Waitable<Result> {
+	protected _resolve : (r:Result) => void = (res) => { throw new Error(`_resolve not yet initialized!`); };
+	protected _reject : (e:any) => void = (res) => { throw new Error(`_reject not yet initialized!`); };
+	protected _exited : boolean = false;
+	#exited : Promise<Result>;
+	
+	/** Close any opened resources, etc */
+	_cleanup() : Promise<void> {
+		return Promise.resolve();
+	}
+	
+	constructor() {
+		this.#exited = new Promise<Result>((resolve,reject) => {
+			this._reject = (e) => {
+				if( this._exited ) {
+					// Ignore it; only the first call counts.
+					return;
+				}
+				this._exited = true;
+				this._cleanup().finally(() => reject(e));
+			}
+			this._resolve = (result) => {
+				if( this._exited ) {
+					// Ignore it; only the first call counts.
+					return;
+				}
+				this._exited = true;
+				this._cleanup().finally(() => resolve(result));
+			};
+		});
+	}
+	
+	wait(): Promise<Result> {
+		return this.#exited;
+	}
+}
+
+export type PossiblyTUIAppSpawner<Context,Instance,InputEvent> = {
+	// Framework will push key events to the app 'asynchronously'
+	inputMode: "push-key-events",
+	spawn(context: Context) : Instance & InputHandler<InputEvent>;
+} | {
+	inputMode: "none",
+	spawn(context: Context) : Instance;
+};
+
+export async function runTuiApp<Result>(
+	spawner: PossiblyTUIAppSpawner<PossiblyTUIAppContext, Waitable<Result>, KeyEvent>,
+	ctx:{
+		stdin  : typeof Deno.stdin,
+		stdout : WritableStreamDefaultWriter
+	},
+	opts: {
+		outputMode: "screen"|"lines"
+	}
+) : Promise<Result> {
+	const textEncoder = new TextEncoder();
+	
+	function getScreenSize() : Vec2D<number> {
+		const cs = Deno.consoleSize();
+		return { x: cs.columns, y: cs.rows };
+	}
+	
+	// Output manager provides a common interface to manage terminal output,
+	// whether in screen or lines mode.
+	const outMan : {
+		enter() : Promise<void>;
+		exit() : Promise<void>;
+		writeOut(text:string) : Promise<void>;
+		setScene(scene:Rasterable) : void;
+	} = opts.outputMode == "lines" ? (() => {
+		let outProm = Promise.resolve();
+		
+		return {
+			enter() { return outProm; },
+			exit() { return outProm; },
+			writeOut(text:string) {
+				return outProm.then(() => ctx.stdout.write(textEncoder.encode(text)));
+			},
+			setScene(scene:Rasterable) {
+				const outCommands = textRaster2ToLines(scene.toRaster({x:0,y:0}, getScreenSize()));
+				for( const command of outCommands ) {
+					outProm = outProm.then(() => ctx.stdout.write(textEncoder.encode(toAnsi(command))));
+				}
+				// outProm = outProm.then(() => ctx.stdout.write(textEncoder.encode(RESET_FORMATTING)));
+			}
+		}
+	})() : (() => {
+		let currentScene : Rasterable = {
+			toRaster(size:Vec2D<number>) : TextRaster2 {
+				return createUniformRaster(size, " ", ansi.RESET_FORMATTING);
+			}
+		}
+		let outProm = Promise.resolve();
+		let inTui = false; // At whatever point in time outProm represents
+		
+		const renderStateMan = new TUIRenderStateManager(ctx.stdout, async (out) => {
+			await outProm;
+			const screenSize = getScreenSize();
+			const raster = currentScene.toRaster(screenSize, screenSize);
+			const outCommands = textRaster2ToDrawCommands(raster);
+			for( const command of outCommands ) {
+				await out.write(textEncoder.encode(toAnsi(command)));
+			}
+			// await ctx.stdout.write(textEncoder.encode(RESET_FORMATTING));
+		});
+		
+		function setMode(newInTui:boolean) : Promise<void> {
+			outProm =
+				newInTui == inTui ? outProm :
+				newInTui ? outProm.then(() => renderStateMan.enterTui()) :
+							  outProm.then(() => renderStateMan.exitTui());
+			inTui = newInTui;
+			return outProm;
+		}
+		
+		return {
+			enter: () => outProm,
+			exit : () => setMode(false),
+			async writeOut(text:string) {
+				await setMode(false);
+				await ctx.stdout.write(textEncoder.encode(text));
+			},
+			setScene(scene:Rasterable) {
+				currentScene = scene;
+				setMode(true);
+				renderStateMan.requestRedraw();
+			}
+		};
+	})();
+
+	let rawModeSet = false;
+	let outManActive = false;
+	
+	async function cleanup() {
+		if( rawModeSet ) {
+			ctx.stdin.setRaw(false);
+			rawModeSet = false;
+		}
+		if( outManActive ) {
+			await outMan.exit();
+			outManActive = false;
+		}
+	}
+	
+	// Finally block is not run when the Deno process is killed with Ctrl+c
+	// (i.e. not in 'raw input' mode, in which case the program handles it).
+	// But this signal handler does seem to be run.
+	// 
+	// I have not figured out how to clean up
+	// when Ctrl+C is pressed while the process
+	// is reading from stdin.
+	Deno.addSignalListener("SIGINT", cleanup);
+	
+	try {
+		await outMan.enter();
+		outManActive = true;
+		
+		let stdin : ReadableStream<Uint8Array>|undefined;
+		if( spawner.inputMode == "push-key-events" ) {
+			ctx.stdin.setRaw(true);
+			rawModeSet = true;
+			stdin = undefined;
+		} else {
+			stdin = ctx.stdin.readable;
+		}
+		
+		const appInstance = spawner.spawn({
+			stdin   : stdin,
+			writeOut: outMan.writeOut,
+			setScene: outMan.setScene,
+		});
+		
+		// App is started!
+		
+		if( spawner.inputMode == "push-key-events" ) {
+			const reader = async () => {
+				const inputHandler = appInstance as unknown as {handleInput(evt:KeyEvent):void};
+				for await( const inputEvent of inputEvents(ctx.stdin.readable) ) {
+					inputHandler.handleInput(inputEvent);
+				}
+			};
+			reader(); // TODO: Cancel somehow when app exits
+		}
+		
+		const exitCode = await appInstance.wait();
+		return exitCode;
+	} finally {
+		await cleanup();
+	}
+}
+
+export abstract class AbstractAppInstance<Input,Result> extends AbstractWaitable<Result> implements Waitable<Result>, InputHandler<Input> {
+	protected _ctx : PossiblyTUIAppContext;
+	
+	constructor(ctx:PossiblyTUIAppContext) {
+		super();
+		this._ctx = ctx;
+	}
+	
+	_handleRunResult(prom:Promise<Result>) {
+		prom.then(this._resolve, this._reject);
+	}
+	
+	handleInput(_rejectinput: Input): void {}
+}
